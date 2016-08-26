d9c96c68e7bf90bc9f665027762a9dcefabdd75b
roy989898 <roy989898@gmail.com>
2016-08-26 14:03:44 +0800

    fix:the dependency of the Dagger2

---
+++ app/src/main/java/poly/pom/trydragger2/MyApplication.java
@@ -0,0 +1,15 @@
+package poly.pom.trydragger2;
+
+import android.app.Application;
+
+/**
+ * Created by User on 26/8/2016.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
