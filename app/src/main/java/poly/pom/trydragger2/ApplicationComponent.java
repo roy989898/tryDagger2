d4478a20df4ed5f686a69921d2eb8b69223b3eae
roy989898 <roy989898@gmail.com>
2016-08-26 13:46:47 +0800

    Lesson1

---
+++ app/src/main/java/poly/pom/trydragger2/ApplicationComponent.java
@@ -0,0 +1,15 @@
+package poly.pom.trydragger2;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by User on 26/8/2016.
+ */
+
+@Singleton
+@Component(modules = {ApplicationComponent.class})
+public interface ApplicationComponent {
+    /*void inject(MainActivity target);*/
+}
---
+++ app/src/main/java/poly/pom/trydragger2/ApplicationModule.java
@@ -0,0 +1,37 @@
+package poly.pom.trydragger2;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ * Created by User on 26/8/2016.
+ */
+
+@Module
+public class ApplicationModule {
+    private Application application;
+
+    public ApplicationModule(Application application) {
+        this.application = application;
+    }
+
+    @Provides
+    @Singleton
+    public Context provideContext() {
+        return application;
+    }
+
+    @Provides
+    @Singleton
+    public SharedPreferences provideSharedPreferences(){
+        return PreferenceManager.getDefaultSharedPreferences(application);
+    }
+
+}
